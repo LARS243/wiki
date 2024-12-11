80660bdf39366e25c46afbbdc808445f507fee9f
LARS243 <arsenka.243@mail.ru>
2024-12-11 14:21:19 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
 import java.net.URLEncoder;
 import java.util.Scanner;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         String input = cin.nextLine();
-        input = encoder(input);
-        System.out.print(input);
+        if (input.isEmpty()){
+            System.out.println("Error");
+        }
+        else{
+            request(input, cin);
+        }
     }
-
-    static  String encoder(String str){
+    static  String encoder(String string){
         try{
-            str = URLEncoder.encode(str, "UTF-8");
+            string = URLEncoder.encode(string, "UTF-8");
+            string = "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=" + string;
         } catch (Exception error){
             System.out.print("Error");
-            str = " ";
+            string = " ";
         }
-        return str;
+        return string;
+    }
+    static void request(String adress, Scanner cin){
+        adress = encoder(adress);
+        InputStream input;
+        FileOutputStream output;
+
     }
 }
