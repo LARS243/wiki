ed5764255ffbf4eda34152d9afcfe95331a128b6
LARS243 <arsenka.243@mail.ru>
2024-12-15 13:29:20 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 import java.net.URL;
 import java.io.InputStream;
 import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,19 +18,44 @@ public class Main {
             request(input, cin);
         }
     }
-    static  String encoder(String string){
+    static String encoder(String string){
         try{
             string = URLEncoder.encode(string, "UTF-8");
             string = "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=" + string;
         } catch (Exception error){
             System.out.print("Error");
-            string = " ";
+            string = "";
         }
         return string;
     }
     static void request(String adress, Scanner cin){
         adress = encoder(adress);
-        InputStream input;
-        FileOutputStream output;
+        InputStream input = null;
+        FileOutputStream output = null;
+        try{
+            URL url = new URL(adress);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+            int responseCode = httpURLConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                input = httpURLConnection.getInputStream();
+
+                File file = new File("file.json");
+                output = new FileOutputStream(file);
+                int byteRead = -1;
+                byte[] buffer = new byte[2048];
+                while((byteRead = input.read(buffer)) != -1){
+                    output.write(buffer, 0, byteRead);
+                }
+            }
+        }catch(IOException e){
+            System.out.println("ERROR" + e.toString());
+        }finally {
+            try {
+                input.close();
+                output.close();
+            } catch (IOException e){}
+
+        }
     }
 }
+
